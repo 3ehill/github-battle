be953460b325721dacc2905e3d28e376848b4414
Erica <3ehill@gmail.com>
2020-10-01 10:18:40 -0500

    Create Github Repo

---
+++ app/index.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import Popular from './components/Popular'
+import Battle from './components/Battle'
+import CheckboxList from './components/CheckboxList'
+class App extends React.Component{
+    render() {
+        return (
+            <div className='container'>
+                <CheckboxList/>
+                <Battle/>
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+   <App />, 
+    document.getElementById('app')
+)
